27abe0fe5a63a40ad0fc07db0ed85b78f59d9b54
Albert Su <albertsu1+github@berkeley.edu>
2024-06-23 23:28:38 -0700

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
 const add = function() {
-	
+  let args = Array.from(arguments);
+  return args.reduce((total, cur) => total + cur);
 };
 
 const subtract = function() {
-	
+	let args = Array.from(arguments);
+  return args.reduce((total, cur) => total - cur);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, cur) => total + cur, 0);
 };
 
 const multiply = function() {
-
+  let args = Array.from(arguments);
+  return args.reduce((total, cur) => total * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
